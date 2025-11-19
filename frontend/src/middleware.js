94f354c70ddf85fab5e72750215744bb70e54c17
MSK1116 <anishkushwaha1116@gmail.com>
2025-11-19 18:26:07 +0545

    asd

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -28,5 +28,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api|.*\\..*).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api|auth/.*|.*\\..*).*)"],
 };
