d5933b0c4531104a24d94f2a2258964359cd3b00
MSK1116 <anishkushwaha1116@gmail.com>
2025-11-22 18:25:19 +0545

    asd

--- frontend/src/app/las/create/Create_form3.jsx
+++ frontend/src/app/las/create/Create_form3.jsx
@@ -21,7 +21,6 @@ const Create_form3 = ({ LMSIN, onDataChange }) => {
       }
     } catch (error) {
       console.error(error);
-    } finally {
     }
   };
 
@@ -36,60 +35,62 @@ const Create_form3 = ({ LMSIN, onDataChange }) => {
   }, [form3]);
   return (
     <>
-      <div className="pt-10 px-10 pb-0">
-        <h1 className="text-xl select-none tracking-wide font-semibold mb-10 px-3 py-1 bg-gray-100 rounded-md w-fit">मालपोतको लागि रोका पत्र</h1>
+      {localData.form3 ? (
+        <div className="pt-10 px-10 pb-0">
+          <h1 className="text-xl select-none tracking-wide font-semibold mb-10 px-3 py-1 bg-gray-100 rounded-md w-fit">मालपोतको लागि रोका पत्र</h1>
 
-        <div className=" flex flex-row items-center justify-between">
-          <div className=" flex flex-row items-center justify-center space-x-5">
-            <div>
-              <Label className={"mb-2"}>कार्यालयको किसिम</Label>
-              <Select onValueChange={(val) => setFrom3((d) => ({ ...d, branchType: val }))} value={form3.branchType || ""} className="mt-2">
-                <SelectTrigger>
-                  <SelectValue placeholder="कार्यालयको किसिम"></SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value={"प्रधान कार्यालय "}>प्रधान कार्यालय</SelectItem>
-                  <SelectItem value={"मुख्य साखा कार्यालय"}>मुख्य साखा कार्यालय</SelectItem>
-                  <SelectItem value={"सेवा केन्द्र"}>सेवा केन्द्र</SelectItem>
-                </SelectContent>
-              </Select>
+          <div className=" flex flex-row items-center justify-between">
+            <div className=" flex flex-row items-center justify-center space-x-5">
+              <div>
+                <Label className={"mb-2"}>कार्यालयको किसिम</Label>
+                <Select onValueChange={(val) => setFrom3((d) => ({ ...d, branchType: val }))} value={form3.branchType || ""} className="mt-2">
+                  <SelectTrigger>
+                    <SelectValue placeholder="कार्यालयको किसिम"></SelectValue>
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value={"प्रधान कार्यालय "}>प्रधान कार्यालय</SelectItem>
+                    <SelectItem value={"मुख्य साखा कार्यालय"}>मुख्य साखा कार्यालय</SelectItem>
+                    <SelectItem value={"सेवा केन्द्र"}>सेवा केन्द्र</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div>
+                {" "}
+                <Label>कार्यालय</Label>
+                <Input className="mt-2" disabled readOnly value={localData?.form1?.branch || " "}></Input>
+              </div>
+              <div className=" w-20">
+                <Label>कोड: </Label>
+                <Input value={form3.branchCode || ""} onChange={(e) => setFrom3((d) => ({ ...d, branchCode: e.target.value }))} className="mt-2"></Input>
+              </div>
+              <div className=" w-30">
+                <Label>पत्र नं. </Label>
+                <Input className="mt-2" value={form3.malpotLetterNo || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotLetterNo: e.target.value }))}></Input>
+              </div>
+              <div className=" w-35">
+                <Label>चलानी नं.</Label>
+                <Input className="mt-2" value={form3.malpotLetterChalaniNo || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotLetterChalaniNo: e.target.value }))}></Input>
+              </div>
             </div>
-            <div>
-              {" "}
-              <Label>कार्यालय</Label>
-              <Input className="mt-2" disabled readOnly value={localData?.form1?.branch || " "}></Input>
-            </div>
-            <div className=" w-20">
-              <Label>कोड: </Label>
-              <Input value={form3.branchCode || ""} onChange={(e) => setFrom3((d) => ({ ...d, branchCode: e.target.value }))} className="mt-2"></Input>
-            </div>
-            <div className=" w-30">
-              <Label>पत्र नं. </Label>
-              <Input className="mt-2" value={form3.malpotLetterNo || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotLetterNo: e.target.value }))}></Input>
-            </div>
-            <div className=" w-35">
-              <Label>चलानी नं.</Label>
-              <Input className="mt-2" value={form3.malpotLetterChalaniNo || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotLetterChalaniNo: e.target.value }))}></Input>
+            <div className=" w-50">
+              <Label className={"mb-2"}>मिति:</Label>
+              <NepaliDateInput value={form3.malpotLetterDate || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotLetterDate: e }))} />
             </div>
           </div>
-          <div className=" w-50">
-            <Label className={"mb-2"}>मिति:</Label>
-            <NepaliDateInput value={form3.malpotLetterDate || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotLetterDate: e }))} />
+          <div className=" mt-5">
+            <div className=" w-1/3">
+              <Label className={"mb-2"}>मालपोत कार्यलय को नाम:</Label>
+              <Input value={form3.malpotOfficeName || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotOfficeName: e.target.value }))}></Input>
+            </div>
+          </div>
+          <div className=" mt-5">
+            <div className=" w-1/3">
+              <Label className={"mb-2"}>पर्तिनिधी :</Label>
+              <Input value={form3.malpotOfficerName || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotOfficerName: e.target.value }))}></Input>
+            </div>
           </div>
         </div>
-        <div className=" mt-5">
-          <div className=" w-1/3">
-            <Label className={"mb-2"}>मालपोत कार्यलय को नाम:</Label>
-            <Input value={form3.malpotOfficeName || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotOfficeName: e.target.value }))}></Input>
-          </div>
-        </div>
-        <div className=" mt-5">
-          <div className=" w-1/3">
-            <Label className={"mb-2"}>पर्तिनिधी :</Label>
-            <Input value={form3.malpotOfficerName || ""} onChange={(e) => setFrom3((d) => ({ ...d, malpotOfficerName: e.target.value }))}></Input>
-          </div>
-        </div>
-      </div>
+      ) : null}
     </>
   );
 };
