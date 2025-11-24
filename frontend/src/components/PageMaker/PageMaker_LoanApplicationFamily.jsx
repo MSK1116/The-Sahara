5b2257a57026cec62ae6158d5c13a9e5526177b8
MSK1116 <anishkushwaha1116@gmail.com>
2025-11-24 11:10:51 +0545

    asd

--- frontend/src/components/PageMaker/PageMaker_LoanApplicationFamily.jsx
+++ frontend/src/components/PageMaker/PageMaker_LoanApplicationFamily.jsx
@@ -12,17 +12,23 @@ export function PageMaker_LoanApplicationFamily(data) {
   // applicant_inlaws_name;
   if (f.applicant_gender == "male") {
     p1 = "बजे";
+    p3 = "नाती ";
   } else if (f.applicant_gender == "female" && f.applicant_maritalStatus == "single") {
     p1 = "बजे";
+    p3 = "नातिनी ";
   } else if (f.applicant_gender == "female" && f.applicant_maritalStatus == "married") {
     p1 = "ससुरा ";
+    p3 = "बुहारी ";
   }
   if (f.applicant_gender == "female" && f.applicant_maritalStatus == "married") {
     p2 = "पति ";
+    p4 = "पतनी ";
   } else if (f.applicant_gender == "female" && f.applicant_maritalStatus == "single") {
     p2 = "बुवा";
+    p4 = "छोरि";
   } else if (f.applicant_gender == "male") {
     p2 = "बुवा ";
+    p4 = "छोरा ";
   }
 
   const landOwnerDetails = [
@@ -54,22 +60,23 @@ export function PageMaker_LoanApplicationFamily(data) {
 
   const landDetails = table7Row.filter((row) => row.govApprovedPrice && row.localApprovedPrice && row.ownerName === f.applicant_name);
   const landDetails2 = table7Row.filter((row) => row.govApprovedPrice && row.localApprovedPrice && row.ownerName === f.approver_applicant_name);
+  const ownerNamesString = [...new Set(f.table7.filter((row) => row.govApprovedPrice && row.localApprovedPrice).map((row) => row.ownerName || "Unknown"))].join(" र ");
 
   const fixedRows = Array.from({ length: 5 }, (_, i) => landDetails[i] || {});
   const fixedRows2 = Array.from({ length: 5 }, (_, i) => landDetails2[i] || {});
   const table7HTM = `
-<table border="1" cellspacing="0" cellpadding="6" width="100%" class="text-xs mt-2 mb-5">
+<table border="1" cellspacing="0" cellpadding="0" width="100%"  class="text-xs mt-2 mb-1.5">
 <thead>
-  <tr>
-    <th rowspan="2" class="w-1/2">जग्गाधनीको विवरण</th>
-    <th colspan="5" class="w-1/2">जग्गाको विवरण</th>
+  <tr class="">
+    <th rowspan="2" class="w-1/2 text-xs p-0 font-semibold ">जग्गाधनीको विवरण</th>
+    <th colspan="5" class="w-1/2 text-xs p-0 font-semibold ">जग्गाको विवरण</th>
   </tr>
-  <tr>
-    <th>जिल्ला</th>
-    <th>न.पा./गा.वि.स.</th>
-    <th>वार्ड नं.</th>
-    <th>कित्ता नं.</th>
-    <th>क्षेत्रफल</th>
+  <tr class="">
+    <th class="text-xs font-semibold">जिल्ला</th>
+    <th class="text-xs font-semibold">न.पा./गा.वि.स.</th>
+    <th class="text-xs font-semibold">वार्ड नं.</th>
+    <th class="text-xs font-semibold">कित्ता नं.</th>
+    <th class="text-xs font-semibold">क्षेत्रफल</th>
   </tr>
 </thead>
 <tbody>
@@ -77,7 +84,7 @@ ${fixedRows
   .map((row, index) => {
     return `
       <tr>
-        <td>${landOwnerDetails[index]}</td>
+        ${index === 0 ? `<td rowspan="5" class="align-top">${landOwnerDetails.join("<br>")}</td>` : ""}
         <td>${row.district || ""}</td>
         <td>${row.palika || ""}</td>
         <td>${row.wardNo || ""}</td>
@@ -86,13 +93,14 @@ ${fixedRows
       </tr>`;
   })
   .join("")}
+
 </tbody>
 </table>`;
   const table7HTM2 = `
-<table border="1" cellspacing="0" cellpadding="6" width="100%" class="text-xs mt-2 mb-5">
+<table border="1" cellspacing="0" cellpadding="0" width="100%" class="text-xs mt-2 mb-5">
 <thead>
   <tr>
-    <th rowspan="2" class="w-1/2">जग्गाधनीको विवरण</th>
+    <th rowspan="2" class="w-1/2 ">जग्गाधनीको विवरण</th>
     <th colspan="5" class="w-1/2">जग्गाको विवरण</th>
   </tr>
   <tr>
@@ -108,7 +116,7 @@ ${fixedRows2
   .map((row, index) => {
     return `
       <tr>
-        <td>${landOwnerDetails2[index]}</td>
+        ${index === 0 ? `<td rowspan="5" class="align-top">${landOwnerDetails2.join("<br>")}</td>` : ""}
         <td>${row.district || ""}</td>
         <td>${row.palika || ""}</td>
         <td>${row.wardNo || ""}</td>
@@ -117,11 +125,12 @@ ${fixedRows2
       </tr>`;
   })
   .join("")}
+
 </tbody>
 </table>`;
   const table2HTML = `
     <h4 class="font-semibold text-center">सहमती दाताको विवरण</h4>
-            <table border="1" cellspacing="0" cellpadding="6" width="100%" class="text-xs mt-2 mb-5">
+            <table border="1" cellspacing="0" cellpadding="0" width="100%" class="text-xs mt-1 mb-1.5">
                 <thead>
 
                     <tr>
@@ -144,7 +153,7 @@ ${fixedRows2
                         </td>
                         <td>${row?.relation}</td>
                         <td></td>
-                        <td class="min-w-48"></td>
+                        <td class=" size-36 "></td>
                     </tr>
                     `;
                       })
@@ -154,54 +163,53 @@ ${fixedRows2
 
   return `
 
-<body class=" flex flex-row items-center">
+<body class=" flex flex-row ">
     <div class="flex w-[10%] h-full">
         <div class=" pr-4 text-center b text-xs ">
             
             <p class="ml-2">
-              जिल्ला __________ न.पा./गा.वि.स ___________ वडा नं. _________ मा बस्ने
+              जिल्ला __________ न.पा./गा.वि.स ___________ वडा नं. _________ मा बस्ने वर्ष _____ को
                 ________________
             </p>
             <p>
-              जिल्ला ___________ न.पा./गा.वि.स ___________ वडा नं. ___________ मा बस्ने
+              जिल्ला ___________ न.पा./गा.वि.स ___________ वडा नं. ___________ मा बस्ने वर्ष _____ को
                 _______________
             </p>
             <p class=" font-semibold">साक्षी :</p>
         </div>
     </div>
-
-    <div class="pl-3 text-justify">
+    <div class="px-3 text-justify">
         <h1 class="my-3">श्री द सहारा लोन सेविंग्स को-ऑपरेटिव सोसाइटी लिमिटेड <br />मुख्य शाखा मलंगवा, सलाही।</h1>
         <h2 class=" text-center">विषयः व्यक्तिगत जमानीको सहमती बारे ।</h2>
         <h3>महोदय,</h3>
-        <h4 class="indent-5">
-            त्यस संस्थाबाद श्री <b>${f.applicant_inlaws_name || ""}</b> को ${p1} श्री ${f.applicant_father_name || ""} को  ${p2} जिल्ला
+        <h4 class="indent-5 text-xs">
+            त्यस संस्था बाट श्री <b>${f.applicant_inlaws_name || ""}</b> को ${p3} श्री ${f.applicant_father_name || ""} को  ${p4} जिल्ला
             ${f.address.permanentOld?.district || ""} गा.वि.स./न. पा. ${f.address.permanentOld.palika || ""} वडा नं.
             ${f.address.permanentOld?.wada || ""} हाल जिल्ला
-            ${f.address.permanent.district || ""} गा.वि.स./न. पा. ${f.address.permanent.palika || ""} वडा नं.
-            ${f.address.permanent.wada || ""} स्थापी ठेगाना भई बसौबास ग्नें वर्ष ${f.age || ""} को श्री ${f.applicant_name || ""} ${f.approver_applicant_name ? f.approver_applicant_name + " र" : ""} को नौउम कर्जां अन्तर्गत जम्मा
+            ${f.address.permanent.district || ""} गा.पा./न. पा. ${f.address.permanent.palika || ""} वडा नं.
+            ${f.address.permanent.wada || ""} स्थापी ठेगाना भई बसौबास ग्नें वर्ष ${convert(f.age || "", "toNp")} को श्री ${f.applicant_name || ""} को नाउँमा ${f.applicantType || "-"} कर्जां अन्तर्गत जम्मा
             रु. ${f2.fiftyPercentMargin ? convert(f2.fiftyPercentMargin, "toNp") : ""} (अक्षरेपी रु.
-            ${f2.fiftyPercentMargin_text || ""} मात्र ) ऋण लिनको लागि सुरक्षण बापत धितो स्वरूप श्री ${f.applicant_name || ""} को नाउँमा जग्गाधनी प्रमाण पूर्जा भई
-            हकभोगमा रहेकी तपसिलमा उल्लेख भए मोजिमको जायजेभा सुरक्षण वापत संस्थालाई धिती लेखि दिएमा धितो जमानी दातार्सँग
+            ${f2.fiftyPercentMargin_text || ""} मात्र ) ऋण लिनको लागि सुरक्षण बापत धितो स्वरूप श्री ${ownerNamesString || ""} को नाउँमा जग्गाधनी प्रमाण पूर्जा भई
+            हकभोगमा रहेकी तपसिलमा उल्लेख भए मोजिमको जायजेभा सुरक्षण वापत संस्थालाई धितो लेखि दिएमा धितो जमानी दातार्सँग
             तल विवरणमा भरिए वमोजिमका हामी नाता सम्बन्धका व्यक्तिहरुको पूर्ण रुपमा सहमती छ। उपरोक्त फर्म/संस्था/व्यक्ति
             ले उल्लेखित कर्ज़ा र त्यसमा
             लागने ब्याज तथा जरिवाना समेत नतिरे/ बुझाएमा सहकारी ऐन तथा संस्थाको नियमावली अनुसार तपसिलमा उल्लेखित धितोदाता
-            श्री ${f.applicant_name || ""} ${f.approver_applicant_name ? f.approver_applicant_name + " र" : ""} को नाडँको
-            जायजेयावाट असुल उपर गरेमा
-            मेरो/ हामो मञ्जुरी छ । पछि उक्त जायजेयामा मेरो हासो पनि हक लागने हो, निजले मात्र
-            धितो लेखि विवैमा मेरो हाग्रो हक जाने होइन भनी कहि कते उजर बाज़ुर गने छैन यदि गरे भने इसे सहमति पत्रलाई आधार
-            मानि मेरो हामरो बाबी खारेज होस् भनी यो सहमतिपत्रको कागजात पटक-पटक ऋण लिन गरेमा मेरो/हाप्रो पूर्ण रूपले
-            मंजुरी हुने अन्नी सहिछाप गरी द सहारा लोन सेविंग्स को-ऑपरेटिव सोसाइटी लिमिटेड ${f.branch || ""} लाई दियाँ /
+            श्री ${ownerNamesString || ""} को नाउँ
+            जायजेथाबाट असुल उपर गरेमा
+            मेरो/हाम्रो मञ्जुरी छ । पछि उक्त जायजेथामा मेरो/हाम्रो पनि हक लागने हो, निजले मात्र
+            धितो लेखि दिदैमा मेरो/हाम्रो हक जाने होइन भनी कहि कतै उजुरी बाज़ुर गर्ने छैन यदि गरे भने यसै सहमति पत्रलाई आधार
+            मानि मेरो/हाम्रो दाबी खारेज होस् भनी यो सहमतिपत्रको कागजात पटक-पटक ऋण लिन गरेमा दाबी पूर्ण रूपले
+            मंजुरी हुने भनि सहिछाप गरी द सहारा लोन सेविंग्स को-ऑपरेटिभ सोसाइटी लिमिटेड, ${f.branchType || ""} ${f.branch || ""} लाई दियाँ /
             दिया । साक्षी
             किनाराको सदर ।
         </h4>
         <div>
-        <h1 class="my-2 font-semibold text-center">तपसिल </h1>
+        <h1 class="my-0.5 font-semibold text-center">तपसिल </h1>
               ${table7HTM}
               ${f.approver_applicant_name ? table7HTM2 : ""}
               ${table2HTML}
         </div>
-                    <p class="mt-3">इति सम्बत्: ${new NepaliDate(new Date()).format("YYYY", "np")} साल ${new NepaliDate(new Date()).format("MMMM", "np")} महिना ${new NepaliDate(new Date()).format("DD", "np")} गते रोज ${new NepaliDate(
+                    <p class="mt-1.5">इति सम्बत्: ${new NepaliDate(new Date()).format("YYYY", "np")} साल ${new NepaliDate(new Date()).format("MMMM", "np")} महिना ${new NepaliDate(new Date()).format("DD", "np")} गते रोज ${new NepaliDate(
     new Date()
   ).format("ddd", "np")} मा शुभम् ।</p>
 
