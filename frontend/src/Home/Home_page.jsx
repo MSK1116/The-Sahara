9e271cdaa11ca696f1261918b1447525e6de8e79
MSK1116 <anishkushwaha1116@gmail.com>
2025-11-26 17:46:44 +0545

    123

--- frontend/src/Home/Home_page.jsx
+++ frontend/src/Home/Home_page.jsx
@@ -36,7 +36,7 @@ const Home_page = ({ sessionAuth0 }) => {
             <Image alt="profile image" className=" border border-white object-center object-cover rounded-full" src={user?.profileImage || "/image_dir/LogoOnly.png"} fill={true}></Image>
           </div>
           <div className=" text-center  mt-4 space-x-1 text-white">
-            <p className=" capitalize font-bold tracking-wider">{sessionAuth0?.user?.nickname || "Guest"}</p>
+            <p className=" uppercase font-bold tracking-wider">{sessionAuth0?.user?.nickname || "Guest"}</p>
             <p className=" text-sm tracking-wide">{user?.officerPost}</p>
             <p className=" text-sm">{user?.officerBranch}</p>
           </div>
